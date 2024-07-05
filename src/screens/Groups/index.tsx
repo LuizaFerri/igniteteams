edaa9b08889643a27883e15ebebb75731e4de5af
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2024-07-04 21:38:29 -0300

    app

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { Container, Text } from "./styles";
+
+export default function Groups() {
+  return (
+    <Container>
+      <Text>Groups</Text>
+      <StatusBar style="auto" />
+    </Container>
+  );
+}
---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #111;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Text = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #fff;
+`;
